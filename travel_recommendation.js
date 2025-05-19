4cc897aff257a97feef871f2eb097f8f355bdbd2
r1thk4 <rithikathilakan@gmail.com>
2025-05-19 23:26:31 +0530

    json

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,34 +1,58 @@
 const resultdiv = document.getElementById("result");
+const searchBtn = document.getElementById("searchBtn");
+const resultBtn = document.getElementById("resetBtn");
 
-const searchKey = document.getElementById("searchBar").value.toLower();
-
-fetch('./travelRecommendation.json')
+function getResults() {
+    const searchKey = document.getElementById("searchBar").value.toLowerCase();
+    resultdiv.innerHTML = "";
+    fetch('./travel_recommendation_api.json')
     .then(response => response.json())
     .then(data => {
-        if( searchKey === "country" || searchKey === "countries") {
+        if (searchKey === "country" || searchKey === "countries") {
             data.countries.forEach(country => {
                 const countryDiv = `<div>
                                 <h2>${country.name}</h2>
-                                <div>${country.cities.map( city =>
-                                `<div>                                    
-                                    <img src="${city.imageUrl}" alt="${city.name}" width="200">
-                                    <h3>${city.name}</h3>
-                                    <p>${city.description}</p>
-                                </div>`).join('')}
+                                <div>${country.cities.map(city =>
+                                    `<div>                                    
+                                        <img src="${city.imageUrl}" alt="${city.name}" width="200">
+                                        <h3>${city.name}</h3>
+                                        <p>${city.description}</p>
+                                    </div>`).join('')}
                                 </div>
                                 </div>`;
-                 resultdiv.innerHTML += countryDiv;                    
+                resultdiv.innerHTML += countryDiv;
             });
-        }
-        else if ( searchKey === "beaches" || searchKey === "beach") {
-            data.beaches.forEach( beach => {
+        } else if (searchKey === "beaches" || searchKey === "beach") {
+            data.beaches.forEach(beach => {
                 const beachDiv = `<div>
-                                    <img src="${beach.imageUrl} alt="beach image" width="200">
+                                    <img src="${beach.imageUrl}" alt="beach image" width="200">
                                     <h3>${beach.name}</h3>
                                     <p>${beach.description}</p>
-                                    </div>`
-                resultdiv.innerHTML += beachDiv
-            })
-        
+                                  </div>`;
+                resultdiv.innerHTML += beachDiv;
+            });
+        } else if (searchKey === "temples" || searchKey === "temple") {
+            data.temples.forEach(temple => {
+                const templeDiv = `<div>
+                                    <img src="${temple.imageUrl}" alt="temple image" width="200">
+                                    <h3>${temple.name}</h3>
+                                    <p>${temple.description}</p>
+                                   </div>`;
+                resultdiv.innerHTML += templeDiv;
+            });
+        }
+        else {
+            resultdiv.innerHTML = `<p> No such keywords. Try keywords like 'beaches', 'countries', 'temples'</p>`;
         }
     })
+    .catch(error => {
+        resultdiv.innerHTML = `<p>Error in fetching: ${error} </p>`
+    });
+};
+
+function resetResults() {
+    resultdiv.innerHTML="";
+}
+
+searchBtn.addEventListener('click', getResults);
+resultBtn.addEventListener('click', resetResults);
