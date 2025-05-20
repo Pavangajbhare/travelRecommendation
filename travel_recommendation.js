fa402dceb73721241e87783346c17d356e7bef29
r1thk4 <rithikathilakan@gmail.com>
2025-05-20 22:33:00 +0530

    style changes

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,11 +1,9 @@
 const resultdiv = document.getElementById("result");
 const searchBtn = document.getElementById("searchBtn");
 const resetBtn = document.getElementById("resetBtn");
-const main = document.querySelector("main")
+const main = document.querySelector("main");
 function getResults() {
-    if (!main.contains(resultdiv)) {
-        main.appendChild(resultdiv);
-    }
+    resultdiv.style.display = "block";
     resultdiv.innerHTML = "";
     const searchKey = document.getElementById("searchBar").value.toLowerCase();
     fetch('./travel_recommendation_api.json')
@@ -16,7 +14,7 @@ function getResults() {
                 const countryDiv = `<div class="countries">
                                 <h2>${country.name}</h2>
                                 <div class="country">${country.cities.map(city =>
-                                    `<div>                                    
+                                    `<div class="result-card">                                    
                                         <img src="${city.imageUrl}" alt="${city.name}">
                                         <h3>${city.name}</h3>
                                         <p>${city.description}</p>
@@ -27,7 +25,7 @@ function getResults() {
             });
         } else if (searchKey === "beaches" || searchKey === "beach") {
             data.beaches.forEach(beach => {
-                const beachDiv = `<div class="beach">
+                const beachDiv = `<div class="beach result-card">
                                     <img src="${beach.imageUrl}" alt="beach image">
                                     <h3>${beach.name}</h3>
                                     <p>${beach.description}</p>
@@ -36,7 +34,7 @@ function getResults() {
             });
         } else if (searchKey === "temples" || searchKey === "temple") {
             data.temples.forEach(temple => {
-                const templeDiv = `<div class="beach">
+                const templeDiv = `<div class="temple result-card">
                                     <img src="${temple.imageUrl}" alt="temple image">
                                     <h3>${temple.name}</h3>
                                     <p>${temple.description}</p>
@@ -57,9 +55,8 @@ function resetResults() {
     const searchKey = document.getElementById("searchBar");
     searchKey.value = "";
     resultdiv.innerHTML = ""; // Just clear the content
-    if (main.contains(resultdiv)) {
-        main.removeChild(resultdiv); // Optional: remove from DOM if needed
-    }
+    resultdiv.style.display = "none";
+    result.style.display = "none";
 }
 
 searchBtn.addEventListener('click', getResults);
