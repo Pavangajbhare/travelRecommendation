65136b28e79696b5cd8f55a97348365f79423260
r1thk4 <rithikathilakan@gmail.com>
2025-05-20 16:05:32 +0530

    more changes

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -13,11 +13,11 @@ function getResults() {
     .then(data => {
         if (searchKey === "country" || searchKey === "countries") {
             data.countries.forEach(country => {
-                const countryDiv = `<div>
+                const countryDiv = `<div class="countries">
                                 <h2>${country.name}</h2>
-                                <div>${country.cities.map(city =>
+                                <div class="country">${country.cities.map(city =>
                                     `<div>                                    
-                                        <img src="${city.imageUrl}" alt="${city.name}" width="200">
+                                        <img src="${city.imageUrl}" alt="${city.name}">
                                         <h3>${city.name}</h3>
                                         <p>${city.description}</p>
                                     </div>`).join('')}
@@ -27,8 +27,8 @@ function getResults() {
             });
         } else if (searchKey === "beaches" || searchKey === "beach") {
             data.beaches.forEach(beach => {
-                const beachDiv = `<div>
-                                    <img src="${beach.imageUrl}" alt="beach image" width="200">
+                const beachDiv = `<div class="beach">
+                                    <img src="${beach.imageUrl}" alt="beach image">
                                     <h3>${beach.name}</h3>
                                     <p>${beach.description}</p>
                                   </div>`;
@@ -36,8 +36,8 @@ function getResults() {
             });
         } else if (searchKey === "temples" || searchKey === "temple") {
             data.temples.forEach(temple => {
-                const templeDiv = `<div>
-                                    <img src="${temple.imageUrl}" alt="temple image" width="200">
+                const templeDiv = `<div class="beach">
+                                    <img src="${temple.imageUrl}" alt="temple image">
                                     <h3>${temple.name}</h3>
                                     <p>${temple.description}</p>
                                    </div>`;
